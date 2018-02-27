2dd1fedb06a44c9a96571b6088862ee1b57060e4
Steve Sweetney <stevesweetney@gmail.com>
2018-02-27 17:28:44 -0500

    Add AverageColor type for DynamicImages that have a known average color. The resize function now returns a vector or AverageColors.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ extern crate image;
 use std::fs;
 use std::io;
 use std::path::Path;
-use image::{DynamicImage, open, ImageError};
-use image::FilterType;
+use image::{DynamicImage, open, FilterType, GenericImage, Pixel};
 
 type GenError = Box<std::error::Error>;
 type GenResult<T> = Result<T, GenError>;
+type AverageColor = (DynamicImage,u8,u8,u8);
+
+/*
+** TODO: Prompt user for source image
+** Process source image (make a grid based on tile size?)
+*/
 
 fn main() {
     println!("Hello, world!");
@@ -25,11 +30,24 @@ fn load_tiles() -> GenResult<Vec<DynamicImage>> {
     Ok(tiles)
 }
 
-fn resize(tiles: Vec<DynamicImage>) -> Vec<DynamicImage> {
-    let mut resized = Vec::new();
+fn resize(tiles: Vec<DynamicImage>) -> Vec<AverageColor> {
+    let mut resized_tiles = Vec::new();
     for tile in tiles {
-        resized.push(tile.resize(50,50,FilterType::Nearest));
+        let resized = tile.resize(50,50,FilterType::Nearest);
+
+        let (mut r,mut g, mut b): (usize,usize,usize) = (0,0,0);
+        for p in resized.pixels() {
+            let channels = p.2.channels();
+            
+            r += channels[0] as usize;
+            g += channels[1] as usize;
+            b += channels[2] as usize;
+        }
+        let p_count = resized.pixels().count();
+        let (r,g,b) = (r/p_count,g/p_count,b/p_count);
+
+        resized_tiles.push((resized,r as u8,g as u8,b as u8));
     }
 
-    resized
+    resized_tiles
 }
